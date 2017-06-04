476362e09ab501e87d0793e7320934c20da142ab
sajkaan <safet.garic@hotmail.com>
2017-06-04 17:06:57 +0200

    Create ControlEventHandler class

---
+++ src/main/java/com/sajkaan/control/ControlEventHandler.java
@@ -0,0 +1,24 @@
+package com.sajkaan.control;
+
+import com.sajkaan.user.User;
+import com.sajkaan.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.core.annotation.HandleAfterSave;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class ControlEventHandler {
+
+    private final UserRepository users;
+
+    @Autowired
+    public ControlEventHandler(UserRepository users) {
+        this.users = users;
+    }
+
+    @HandleAfterSave
+    public void setLastModifiedAfterSave(Control control) {
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = users.findByUsername(name);
+        control.setLastModifiedBy(user);
+    }
+}
